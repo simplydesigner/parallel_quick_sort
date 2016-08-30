45ef1d2dced4dab35e77ed5eebad5538ecc745eb
simplydesigner <simplydesigner@ya.ru>
2016-08-30 07:53:12 +0300

    Initial Commit

---
+++ parallel_quick_sort/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  parallel_quick_sort
+//
+//  Created by simplydesigner on 8/30/16.
+//  Copyright © 2016 simplydesigner. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
